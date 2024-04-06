7c4c8bb686924b09e4b75cbec8eab74cd2d84d1a
Rodry <38259440+ImRodry@users.noreply.github.com>
2024-04-06 16:24:17 +0100

    Create types.scala

---
+++ src/types.scala
@@ -0,0 +1,8 @@
+type Board = List[List[Char]]
+type Coord2D = (Int, Int) //(row, column)
+object Direction extends Enumeration {
+  type Direction = Value
+  val North, South, East, West,
+  NorthEast, NorthWest, SouthEast, SouthWest = Value
+
+}
