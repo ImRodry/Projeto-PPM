10ae7f4abdeee9d3ed243b89448beb55fc553add
Rodry <38259440+ImRodry@users.noreply.github.com>
2024-04-10 22:18:06 +0100

    feat: cores

--- src/TextUI.scala
+++ src/TextUI.scala
@@ -8,7 +8,8 @@ object TextUI {
     println("Bem-vindo ao jogo de palavras cruzadas!")
     println("1. Selecionar palavra")
     println("2. Reiniciar jogo")
-    println("3. Sair")
+    println("3. Alterar cor do texto")
+    println("4. Sair")
   }
 
   def getInput(prompt: String): String = {
@@ -43,6 +44,40 @@ object TextUI {
           printBoard(board)
           runGame(board) // Continue running the game with the same board
         case "3" =>
+          println("Escolha uma das cores abaixo")
+          println("1. Preto")
+          println("2. Vermelho")
+          println("3. Verde")
+          println("4. Amarelo")
+          println("5. Azul")
+          println("6. Roxo")
+          println("7. Ciano")
+          println("8. Branco")
+          println("9. Reset")
+          getInput("Escolha uma cor") match {
+            case "1" =>
+              println(Console.BLACK)
+            case "2" =>
+              println(Console.RED)
+            case "3" =>
+              println(Console.GREEN)
+            case "4" =>
+              println(Console.YELLOW)
+            case "5" =>
+              println(Console.BLUE)
+            case "6" =>
+              println(Console.MAGENTA)
+            case "7" =>
+              println(Console.CYAN)
+            case "8" =>
+              println(Console.WHITE)
+            case "9" =>
+              println(Console.RESET)
+            case _ =>
+              println("Cor inválida")
+          }
+          runGame(board) // Continue running the game with the same board
+        case "4" =>
           println("Obrigado por jogar!")
         case _ =>
           println("Opção inválida")
